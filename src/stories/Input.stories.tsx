de65d42b9e221fc0b44bc9b29272b30e4f5998c7
Pavel Solovev <pavelsol1990@gmail.com>
2022-02-11 21:53:47 +0300

    add controlled Input CheckBox Select

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -31,3 +31,31 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 
   return <><input ref={inputRef}/> <button onClick={save}>save</button> - actual value: {value} </>
 }
+export const ControlledInput = () => {
+  const [value, setValue] = useState('')
+  const onChange = (event:ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value)
+  }
+  return <input value={value} onChange={onChange}/>
+}
+
+export const ControlledCheckBox = () => {
+  const [value, setValue] = useState(true)
+  const onChange = (event:ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.checked)
+  }
+  return <input type={"checkbox"} checked={value} onChange={onChange}/>
+}
+
+export const ControlledSelected = () => {
+  const [value, setValue] = useState<string | undefined>(undefined)
+  const onChange = (event:ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value)
+  }
+  return <select>
+    <option>none</option>
+    <option value={1}>Minsk</option>
+    <option value={2}>Moscow</option>
+    <option value={3}>Kiev</option>
+  </select>
+}
