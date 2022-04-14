a9a073a840ead81408d7a9c9364590d6eb0341ec
Pavel Solovev <pavelsol1990@gmail.com>
2022-04-14 20:53:46 +0300

    add React update UseEffect.stories.tsx

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -5,31 +5,58 @@ export default {
 }
 
 export const SimpleExample = () => {
+    const a = new Date()
     const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
+    const [time, setTime] = useState([0, 0, 0])
+
 
     console.log('SimpleExample')
 
     useEffect(() => {
-        console.log('useEffect every render')
-        document.title = counter.toString()
-    }, )
+        setInterval(() => {
+            debugger
+            setTime(state => [state[0], state[1], state[2] + 1])
+            if (time[2] >= 60) {
+                setTime(state => [state[0], state[1] + 1, state[2] = 0])
+            }
+            if (time[1] >= 60) {
+                setTime(state => [state[0] + 1, state[1] = 0, state[2] = 0])
+            }
+            if (time[0] >= 24) {
+                setTime(state => [state[0] = 0, state[1] = 0, state[2] = 0])
+            }
 
-    useEffect(() => {
-        console.log('useEffect only first render (componentDidMount)')
-        document.title = counter.toString()
-    }, [])
+            // setTime((state) => {
+            //     return ([state[2] > 59 ? 0 & state[1] + 1 : state[2] + 1
+            //         , state[1] > 59 ? 0 & state[0] + 1 : state[1]
+            //         , state[0] > 23 ? 0 : state[0]])
+            // })
+        }, 1000)
+    }, [time[2]])
 
-    useEffect(() => {
-        console.log('useEffect first render and every counter change')
-        document.title = counter.toString()
-    }, [counter])
+    // useEffect(() => {
+    //     console.log('useEffect every render')
+    //     document.title = counter.toString()
+    // }, )
+    //
+    // useEffect(() => {
+    //     console.log('useEffect only first render (componentDidMount)')
+    //     document.title = counter.toString()
+    // }, [])
+    //
+    // useEffect(() => {
+    //     console.log('useEffect first render and every counter change')
+    //     document.title = counter.toString()
+    // }, [counter])
 
     return (
         <>
             Hello, {counter} {fake}
-            <button onClick={()=> setFake(fake + 1)}>fake+</button>
-            <button onClick={()=> setCounter(counter + 1)}>counter+</button>
+            <button onClick={() => setFake(fake + 1)}>fake+</button>
+            <button onClick={() => setCounter(counter + 1)}>counter+</button>
+
+            Time: {time[0]}:{time[1]}:{time[2]}
         </>
     );
 };
