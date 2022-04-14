cfb4d5f761db063fdade660d3e9ad73fe5ad1776
Pavel Solovev <pavelsol1990@gmail.com>
2022-04-14 15:33:29 +0300

    add React UseState.stories.tsx

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+export default {
+    title: 'useState demo',
+}
+
+function generateData() {
+    return 1
+}
+
+export const Example1 = () => {
+    console.log('Example1')
+    const [counter, setCounter] = useState<number>(generateData)
+
+    // const changer = (state:number) => state + 1
+    return (
+        <>
+            <button onClick={()=> setCounter(state => state + 1)}>+</button>
+            {counter}
+        </>
+    );
+};
